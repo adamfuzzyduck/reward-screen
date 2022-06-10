722fb79fc2c03bf5bd982dc7854253e1eaf7e705
abkozlowski <46653737+abkozlowski@users.noreply.github.com>
2022-06-10 16:26:30 +0100

    commit of fading in text

--- js/main.js
+++ js/main.js
@@ -23,3 +23,23 @@ $(".turbine-status__temp").on("click", function () {
 
 // Cycle through error messages
 
+// $(".action-required__action-text").each(function(index){
+//     if (!$( this ).hasClass("complete")) {
+//         $(this).delay(3000*index).fadeIn(3000).delay(300).fadeOut(1000);
+//         console.log(this);
+//     }
+// });
+
+$(function() {
+    var i = 0;
+    var delay = 3000; 
+    var items = $(".action-required__action-text-wrapper .action-required__action-text");
+    items.eq(0).show();
+    var len = items.length;
+    setInterval(function() {
+        //items.fadeOut().eq(++i % len).fadeIn();
+        items.filter(":visible").fadeOut(function() {
+            items.eq(++i % len).fadeIn();
+        });
+    }, delay);
+});
