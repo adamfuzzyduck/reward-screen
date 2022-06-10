dde6062561b22ca59aab386460d770af44b182e0
abkozlowski <46653737+abkozlowski@users.noreply.github.com>
2022-06-10 14:51:26 +0100

    builds basic click events

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(".turbine-status__temp").on("click", function () {
+    $( this ).addClass("complete");
+    $('.turbine-status__exclamation--temp').hide();
+    $('.turbine-status__okay-text--temp').show();
+    $('.turbine-status__triangle--temp').addClass("complete");
+  });
+
+  $(".turbine-status__oil-pressure").on("click", function () {
+    $( this ).addClass("complete");
+    $('.turbine-status__exclamation--oil-pressure').hide();
+    $('.turbine-status__okay-text--oil-pressure').show();
+    $('.turbine-status__triangle--oil-pressure').addClass("complete");
+  });
+
+  $(".turbine-status__bolt-tension").on("click", function () {
+    $( this ).addClass("complete");
+    $('.turbine-status__exclamation--bolt-tension').hide();
+    $('.turbine-status__okay-text--bolt-tension').show();
+    $('.turbine-status__triangle--bolt-tension').addClass("complete");
+  });
